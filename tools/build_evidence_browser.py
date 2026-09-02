385b25234e95ff9a1cbe70ad688a0ca09ce220cd
4laric <alaric.mckenzie.boone@gmail.com>
2026-09-02 03:07:40 -0400

    evidence: bind repeated upgrade materials (#1345)

--- tools/build_evidence_browser.py
+++ tools/build_evidence_browser.py
@@ -141,6 +141,7 @@ def wiki_tables(path: str = WIKI_AUDIT) -> tuple[list[dict[str, str]], list[dict
     acquisition_manifests = (
         "eldenpedia-deathroot-pages.tsv",
         "eldenpedia-item-acquisition-pages.tsv",
+        "eldenpedia-upgrade-material-pages.tsv",
         "eldenpedia-sacred-tear-pages.tsv",
         "eldenpedia-seedbed-curse-pages.tsv",
         "eldenpedia-shabriri-grape-pages.tsv",
